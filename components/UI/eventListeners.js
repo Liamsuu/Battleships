c3b45abfde7eab3409217120a9c1c10092f2fabb
Liam <liamsuu14974@gmail.com>
2024-06-27 18:10:34 +0100

    Create functionality to stop clicks after win

--- components/UI/eventListeners.js
+++ components/UI/eventListeners.js
@@ -1,25 +1,40 @@
 import renderBoard from "./renderBoard";
-export default function inititializeBoardClicks(playerObj, playerUI, divElems) {
-  divElems.forEach((elem) => {
-    elem.addEventListener("click", () => {
-      const dataVal = elem.dataset.position;
+export default class UserInterface {
+  constructor() {}
+  inititializeBoardClicks(playerObj, playerContent, divElems) {
+    divElems.forEach((elem) => {
+      elem.onclick = () => {
+        const dataVal = elem.dataset.position;
 
-      // get bot board, add hit to position
-      playerObj.playerBoard.receiveAttack(
-        Number(dataVal.charAt(0)),
-        Number(dataVal.charAt(2))
-      );
+        // get bot board, add hit to position
+        playerObj.playerBoard.receiveAttack(
+          Number(dataVal.charAt(0)),
+          Number(dataVal.charAt(2))
+        );
 
-      // re-render bot board
-      renderBoard(playerUI, playerObj.playerBoard);
-      if (playerObj.playerBoard.allShipsSunk() === true) {
-        // later change this to either play or bot winner by checking playerObj if real player === true
-        if (playerObj.realPlayer === false) {
-          alert("You won!");
-        } else {
-          alert("The bot won!");
+        // re-render bot board
+        renderBoard(playerContent, playerObj.playerBoard);
+        if (playerObj.playerBoard.allShipsSunk() === true) {
+          // announce winner
+          if (playerObj.realPlayer === false) {
+            alert("You won!");
+            this.removeClickableEvents();
+          } else {
+            alert("The bot won!");
+            this.removeClickableEvents();
+          }
         }
-      }
+      };
     });
-  });
+  }
+  removeClickableEvents() {
+    const playerDivs = document.querySelector("#player-content");
+    playerDivs.querySelectorAll("div").forEach((elem) => {
+      elem.onclick = null;
+    });
+    const botDivs = document.querySelector("#bot-content");
+    botDivs.querySelectorAll("div").forEach((elem) => {
+      elem.onclick = null;
+    });
+  }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import "./index.css";
 import renderBoard from "../components/UI/renderBoard";
 import createDataPos from "../components/UI/positionData";
 import inititializeBoardClicks from "../components/UI/eventListeners";
+import UserInterface from "../components/UI/eventListeners";
+import resetGame from "../components/UI/reset";
+const resetBtn = document.querySelector("#restart-btn");
+const randomiseBtn = document.querySelector("#random-btn");
 
 const userPlayer = new Player(true);
 const botPlayer = new Player(false);
@@ -14,22 +18,29 @@ userPlayer.playerBoard.setShipPosition(4, 5, "vertical", 3);
 botPlayer.playerBoard.setShipPosition(0, 4, "horizontal", 2);
 userPlayer.playerBoard.setShipPosition(9, 5, "vertical", 3);
 // update user UI to match gameboard
-const usersUI = document.querySelector("#player-content");
+const usersContent = document.querySelector("#player-content");
 for (let loops = 0; loops < 100; loops++) {
-  usersUI.appendChild(createDataPos(loops));
+  usersContent.appendChild(createDataPos(loops));
 }
 
-renderBoard(usersUI, userPlayer.playerBoard);
-const userDivs = usersUI.querySelectorAll("div");
-inititializeBoardClicks(userPlayer, usersUI, userDivs);
+renderBoard(usersContent, userPlayer.playerBoard);
+const userDivs = usersContent.querySelectorAll("div");
+const userUI = new UserInterface();
+userUI.inititializeBoardClicks(userPlayer, usersContent, userDivs);
 
 // update bots UI to match gameboard
-const botsUI = document.querySelector("#bot-content");
+const botsContent = document.querySelector("#bot-content");
 for (let loops = 0; loops < 100; loops++) {
-  botsUI.appendChild(createDataPos(loops));
+  botsContent.appendChild(createDataPos(loops));
 }
-renderBoard(botsUI, botPlayer.playerBoard);
+renderBoard(botsContent, botPlayer.playerBoard);
 
 // event listener to enemies board for clicks
-const botDivs = botsUI.querySelectorAll("div");
-inititializeBoardClicks(botPlayer, botsUI, botDivs);
+const botDivs = botsContent.querySelectorAll("div");
+const botUI = new UserInterface();
+botUI.inititializeBoardClicks(botPlayer, botsContent, botDivs);
+
+resetBtn.addEventListener(
+  "click",
+  resetGame(userPlayer, usersContent, botPlayer, botsContent)
+);
